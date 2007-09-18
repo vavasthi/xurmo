452ce7157b8fe8b923d36c8bb0a02451b07c980e
yaniv <yaniv@86810559-af23-0410-8aff-8d2cc78d7198>
2007-09-18 17:40:15 +0000

    Committed
    git-svn-id: file:///home/vavasthi/web/svn/xurmo@74 86810559-af23-0410-8aff-8d2cc78d7198

---
+++ XurmoConnect/XurmoConnect-ejb/src/java/com/xurmo/connect/user/XurmoShortMessageServiceInterface.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2007, Xurmo.com. All rights reserved.
+ *
+ * File name                : XurmoShortMessageServiceInterface.java
+ * Created on               : September 16, 2007, 7:45 PM
+ * Created by               : xurmo
+ *
+ */
+
+package com.xurmo.connect.user;
+
+/**
+ *
+ * @author xurmo
+ */
+public class XurmoShortMessageServiceInterface {
+  
+  /** Creates a new instance of XurmoShortMessageServiceInterface */
+  private XurmoShortMessageServiceInterface() {
+  }
+  public static void sendSMS(XurmoRequestToJoinInbox rtjm) {
+    
+  }
+}
